6eb8d9fe2f1786d636c2c5b946556d5d5510bcb3
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2020-12-31 12:06:13 +0100

    javascript and jquery

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,20 +1,58 @@
 ﻿    /* site.js */
 
 
-var ele = document.getElementById("username");
-ele.innerHTML = "Kristof Andrasi";
+////////////////////
+// raw javaScript //
+////////////////////
+/*
+(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Kristof Andrasi";
 
 
+// JavaScript event
 
-var main = document.getElementById("main");
-main.onmouseenter = function () {
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
 
-    main.style = "background-color: #888;";
+        main.style = "background-color: #888;";
 
-};
+    };
 
-main.onmouseleave = function () {
+    main.onmouseleave = function () {
 
-    main.style = "";
+        main.style = "";
 
-};
+    };
+}) (); 
+*/
+
+//////////////////
+// using jQuery //
+//////////////////
+
+(function () {
+
+    var ele = $("#username");
+    ele.text("Kristof Andrasi");
+
+
+    var main = $("#main");
+    main.on( "mouseenter" , function () {
+
+        main.css("background-color", "#888");
+    });
+
+    main.on("mouseleave", function () {
+
+        main.css("background-color", "");
+    });
+
+    var menuitems = $("ul.menu li a");
+    menuitems.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+    })
+
+})();
+
