e4fdb3e1159a7fa00228fb066a75c7436efa7100
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2020-12-31 08:37:42 +0100

    adding JavaScript and implementing JS events

---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿    /* site.js */
+
+
+var ele = document.getElementById("username");
+ele.innerHTML = "Kristof Andrasi";
+
+
+
+var main = document.getElementById("main");
+main.onmouseenter = function () {
+
+    main.style = "background-color: #888;";
+
+};
+
+main.onmouseleave = function () {
+
+    main.style = "";
+
+};
