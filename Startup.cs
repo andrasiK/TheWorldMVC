36b0374f64d04ab60d30dc4c204d68d2ecd5c7cb
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2021-01-04 08:54:25 +0100

    Adding MVC

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace The_World
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +29,7 @@ namespace The_World
 
             app.UseStaticFiles();
 
+       
             app.UseRouting();
 
            
