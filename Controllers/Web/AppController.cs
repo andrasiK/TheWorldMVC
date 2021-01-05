440c055226dc6d341dbfb9b542ffb2f45b75f019
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2021-01-05 14:15:12 +0100

    More Views added

--- Controllers/Web/AppController.cs
+++ Controllers/Web/AppController.cs
@@ -13,5 +13,15 @@ namespace The_World.Controllers.Web
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
---
+++ Views/App/About.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace The_World.Views.App
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
